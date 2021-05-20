2445ed442cf247260f48e0642db0913042134fe5
Rawan Almasalmeh <>
2021-05-20 21:43:54 +0000

    fixed

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-//Run this code first and examine the error message.
-//Fix the syntax error then run the code again to check your work.
-
 let launchReady = false;
+let crewReady = false;
 let fuelLevel = 17000;
+let crewStatus = true;
+let computerStatus = 'green';
 
-if (fuelLevel = 20000) {
+if (fuelLevel >= 20000) {
    console.log('Fuel level cleared.');
    launchReady = true;
 } else {
    console.log('WARNING: Insufficient fuel!');
    launchReady = false;
 }
+console.log("launchReady = ", launchReady);
+
+if (crewStatus && computerStatus === 'green'){
+   console.log('Crew & computer cleared.');
+   crewReady = true;
+} else {
+   console.log('WARNING: Crew or computer not ready!');
+   crewReady = false;
+}
+console.log("crewReady = ", crewReady);
